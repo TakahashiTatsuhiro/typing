6f0b40445b342d2cfd5950dfa2ec498558733a65
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2023-11-17 08:50:19 +0900

    loginフォーム原型まで

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = 3001; //vite側を3000にするため
+
+// CORS（Cross-Origin Resource Sharing）の設定
+const cors = require('cors');
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!!');
---
+++ frontend/src/components/LoginForm.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    // ここでサーバーにログイン情報を送信する処理を実装
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="ユーザー名"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="パスワード"
+      />
+      <button type="submit">ログイン</button>
+    </form>
+  );
+};
+
+export default LoginForm;
